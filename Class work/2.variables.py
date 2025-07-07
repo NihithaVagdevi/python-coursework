13deaa9769e11ec71cc220bd1fbf85ca7572f4ee
NihithaVagdevi <nihithavagdevi@gmail.com>
2025-07-07 20:15:59 +0530

    variables

---
+++ Class work/2.variables.py
@@ -0,0 +1,26 @@
+#Basic Variable Assignment
+product_name = "Laptop"
+price = 45000
+in_stock = True
+print(product_name, price, in_stock)
+
+#Multiple Assignment
+a, b, c = 10, 20, 30
+print(a, b, c)
+x = y = z = 100
+print(x, y, z)
+
+#Reassignment
+x = 5
+x = 10
+print(x)
+
+#Swapping Variables
+a, b = 5, 10
+a, b = b, a
+print(a, b)
+
+#Deleting Variables
+x = 100
+print(x)
+del x
