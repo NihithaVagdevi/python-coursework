76f7214e13ecf0092af873357bf557577e869df2
NihithaVagdevi <nihithavagdevi@gmail.com>
2025-08-12 14:26:53 +0530

    control statements

---
+++ Class work/17.builtin_modules.py
@@ -0,0 +1,27 @@
+#System
+# import sys
+# print(sys.argv)
+# print(sys.path)
+#  print(sys.version)
+# print("Before")
+# print(sys.exit())
+# print("After")
+
+#Platform
+# import platform
+# print(platform.system())
+# print(platform.release())
+# print(platform.processor())
+
+#Mathematical
+import math
+print(math.pi)
+print(math.e)
+print(math.sqrt(4))
+print(math.pow(3,3))
+print(round(12.3))
+print(math.ceil(12.3))
+print(math.floor(12.7))
+print(abs(-3))
+print(math.fabs(-63))
+print(math.factorial(6))
--- Practice work/control_stats.py
+++ Practice work/control_stats.py
@@ -307,3 +307,10 @@
 #  * *
 #   *
 
+a=int(input())
+b=int(input())
+c=min(a,b)
+for i in range(c,0,-1):
+    if a%i==0 and b%i==0:
+        print(f'GCD ({a},{b}):{i}')
+        break
